5c9a0fbd73cdcae98c24a7df9e174ec5d5dcc52d
Hoa Vu <hoavu2491@gmail.com>
2023-04-03 23:20:42 +0700

    chores: init

---
+++ modules/storage/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  description = "project_id"
+  type        = string
+  default     = "[Project ID]"
+}
+variable "region" {
+  description = "region"
+  type        = string
+  default     = "us-east1"
+}
+variable "zone" {
+  description = "zone"
+  type        = string
+  default     = "[Zone]"
+}
